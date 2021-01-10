7d23a86a11e5d80e816ce904cfa6c965ccd6c189
toru-Major <tohru7718@gmail.com>
2021-01-10 15:45:28 +0900

    Rubocopの実施

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,10 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :email, uniqueness: { case_sensitive: false }
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
 end
-
-
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {'test1234TEST'}
-    password_confirmation {password}
-    last_name             {"寺原"}
-    first_name            {"徹"}
-    last_name_kana        {"テスト"}
-    first_name_kana       {"タロウ"}
-    birthday              {Faker::Date.between(from: '2014-09-23', to: '2014-09-25')}
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { 'test1234TEST' }
+    password_confirmation { password }
+    last_name             { '寺原' }
+    first_name            { '徹' }
+    last_name_kana        { 'テスト' }
+    first_name_kana       { 'タロウ' }
+    birthday              { Faker::Date.between(from: '2014-09-23', to: '2014-09-25') }
   end
 end
