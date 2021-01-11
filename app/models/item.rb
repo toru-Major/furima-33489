f982a7410e8329ef3c1fc86ca0c2034e4c4e9bce
toru-Major <tohru7718@gmail.com>
2021-01-11 12:46:35 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,17 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :detail
+    validates :price, numericality: {greater_than: 300,
+                                     less_than: 9999999},
+                      format: {with: \d}
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :days_to_ship_id
+  end
 end
