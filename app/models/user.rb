98128dd4125cacaa0ec01a5eeb085548165841a2
toru-Major <tohru7718@gmail.com>
2021-01-11 09:32:58 +0900

    validationの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  with_options presence: true do
 
-  validates :nickname, presence: true
+  validates :nickname
   validates :email, uniqueness: { case_sensitive: false }
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birthday, presence: true
+  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/ }
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/ }
+  validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birthday
+
+  end
 end
