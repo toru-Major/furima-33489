a131192e10906ca5d0f22edc87137b8e1b63fd3d
toru-Major <tohru7718@gmail.com>
2021-01-11 17:17:40 +0900

    バリデーションの記載

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,17 +1,26 @@
 class Item < ApplicationRecord
-  belongs_to :user
-  has_one_attached :image
 
   with_options presence: true do
     validates :name
     validates :detail
-    validates :price, numericality: {greater_than: 300,
-                                     less_than: 9999999},
-                      format: {with: \d}
+    validates :image
+    validates :price, numericality: {greater_than: 300, less_than: 999999}
+  end
+  with_options numericality: { other_than: 1 } do
     validates :category_id
     validates :condition_id
     validates :shipping_fee_id
     validates :prefecture_id
-    validates :days_to_ship_id
+    validates :days_to_ship_id 
   end
+
+  belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :days_to_ship
 end
