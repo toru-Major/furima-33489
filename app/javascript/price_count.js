8e9e6484bafaa7d678a2fa48d71a288e678a9edb
toru-Major <tohru7718@gmail.com>
2021-01-11 18:16:08 +0900

    販売手数料、利益の計算と表示

---
+++ app/javascript/price_count.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price")
+  priceInput.addEventListener("input", () =>{
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+    const addProfit = document.getElementById("profit");
+    addProfit.innerHTML = Math.floor(inputValue * 0.9);
+  })
+});
