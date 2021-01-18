1e4301fa794d11d4940c3bd0417fef702a371257
toru-Major <tohru7718@gmail.com>
2021-01-18 19:10:43 +0900

    同一コードの記述量を減らす

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,46 +3,42 @@ class OrdersController < ApplicationController
   before_action :move_to_top
 
   def index
-    @item = Item.find(params[:item_id])
-    @order = Order.new
-    @address = Address.new
     @item_order = ItemOrder.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @item_order = ItemOrder.new(order_params)
     if @item_order.valid?
       pay_item
       @item_order.save
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
-      @order = Order.new
-      @address = Address.new
       render action: :index
     end
   end
 
   private
-
+  
   def move_to_top
-    item = Item.find(params[:item_id])
-    redirect_to root_path if current_user.id == item.user_id || item.order.present?
+    @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id || @item.order.present?
+      redirect_to root_path
+    end
   end
 
   def order_params
-    item = Item.find(params[:item_id])
+    @item = Item.find(params[:item_id])
     params.require(:item_order).permit(:postal_code, :prefecture, :city, :block, :building, :phone)
-          .merge(user_id: current_user.id, item_id: item.id, token: params[:token])
+                               .merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
 
   def pay_item
-    @item = Item.find(params[:item_id])
-    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: @item.price,
       card: order_params[:token],
       currency: 'jpy'
-    )
+      )
   end
 end
