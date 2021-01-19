3c309cc92856243d3bfc85f965f5c40d3bcd7fa3
toru-Major <tohru7718@gmail.com>
2021-01-19 15:10:08 +0900

    出品時の値段のvalidationの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
     validates :name
     validates :detail
     validates :image
-    validates :price, numericality: { greater_than: 300, less_than: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
+    validates :price, numericality: { greater_than: 299, less_than: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
 
     with_options numericality: { other_than: 1 } do
       validates :category_id
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -86,7 +86,7 @@ RSpec.describe Item, type: :model do
       it '価格の範囲が、￥300~9,999,999の間でなければ出品できない' do
         @item.price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price must be greater than 300')
+        expect(@item.errors.full_messages).to include('Price must be greater than 299')
       end
       it '価格の範囲が、￥300~9,999,999の間でなければ出品できない' do
         @item.price = 10_000_000
